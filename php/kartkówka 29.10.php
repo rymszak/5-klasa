50662e8d5c59b67a9c295009086d4220c37f6972
rymszak <102522109+rymszak@users.noreply.github.com>
2024-10-29 10:21:17 +0100

    Create kartkówka 29.10.php

---
+++ php/kartkówka 29.10.php
@@ -0,0 +1,37 @@
+<?php
+$imie=$_POST["imie"];
+$szkola=$_POST["szkola"];
+$rola=$_POST["rola"];
+$adult=$_POST["pelnoletni"];
+if($adult==NULL){
+    $adult="niepełnoletni";
+}
+
+echo "Jesteś ".$imie.",<br>Jesteś ".$rola." w ". $szkola.".<br>Z formularza wynika, że jesteś ".$adult;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="wynik.php" method="post">
+    <label for="imie">imie<input type="text" name="imie">
+    </label><br>
+   <input type="radio" name="rola" id="uczen" value="uczeniem/uczennicą">uczeń
+<br>   <input type="radio" name="rola" id="rodzic" value="rodzicem">rodzic
+ <br>  <input type="radio" name="rola" id="nauczyciel" value="nauczycielem/ką">nauczyciel 
+  <br>  <select name="szkola" id="szkola">
+    <option value="szkole podstawowej">szkoła podstawowa</option>
+    <option value="szkole średniej">szkoła średnia</option>
+    <option value="studiach wyższych">studia wyższe</option>
+    </select>
+ <br>   <input type="checkbox" name="pelnoletni" value="pełnoletni">pełnoletniość
+  <br>  <button type="submit">wyślij</button>
+</form>
+</body>
+</html>
