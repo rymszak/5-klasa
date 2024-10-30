bb74d459521b03ec6e08adc04a2ec8253ed1ba36
rymszak <102522109+rymszak@users.noreply.github.com>
2024-10-30 12:51:19 +0100

    Update strona.php

--- php/sesje/strona.php
+++ php/sesje/strona.php
@@ -22,3 +22,41 @@ if(isset($_SESSION['kosz']))
 </form> 
 </body>
 </html>
+
+
+
+<!DOCTYPE html>
+<?php
+session_start();
+if(isset($_SESSION['login']))
+{
+    header('location:login.php');
+    exit();
+}
+
+
+
+elseif(isset($_SESSION['kosz'])){
+$_SESSION['kosz']=$_POST['towar'];
+    header("location:kosz.php");
+   
+}else{
+echo '<p><button name="out">wyloguj</button></p>';
+}
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>strona</title>
+</head>
+<body>
+   <form action="kosz.php" method="post">
+    auto<input type="checkbox" name="towar[]" value="auto"><br>
+    rower<input type="checkbox" name="towar[]" value="rower"><br>
+    samolot<input type="checkbox" name="towar[]" value="samolot">
+<br>    helikopter<input type="checkbox" name="towar[]" value="heli">
+<br> <button type="submit">zapisz</button>
+</form> 
+</body>
+</html>
