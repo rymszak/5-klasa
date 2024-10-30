3e5248f8583a570e386aa59533e9efcb34bdf499
rymszak <102522109+rymszak@users.noreply.github.com>
2024-10-30 12:51:00 +0100

    Update kosz.php

--- php/sesje/kosz.php
+++ php/sesje/kosz.php
@@ -26,3 +26,36 @@ if(isset($_SESSION['kosz']))
 
 </body>
 </html>
+
+
+
+<!DOCTYPE html>
+<?php 
+session_start();
+if(isset($_SESSION['login']))
+{
+ header('location:login.php');
+ exit();   
+}
+
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kosz</title>
+</head>
+<body>
+
+    <?php
+    echo "witaj ";
+    // echo $_SESSION['login'];
+    echo "twój koszyk to<br><ol>";
+  foreach($_POST['towar'] as $el){
+    echo "<li>$el</li>";
+  }
+  echo "</ol>";
+    ?>
+
+</body>
+</html>
