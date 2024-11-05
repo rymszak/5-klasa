de3c7d4190dbf448e9773225526695e7451b7d67
rymszak <102522109+rymszak@users.noreply.github.com>
2024-11-05 12:20:17 +0100

    Update 11_05_24.php

--- php/11_05_24.php
+++ php/11_05_24.php
@@ -99,3 +99,43 @@ while($wiersze=mysqli_fetch_row($zap)){
 </html>
 
 
+_____________________
+<?php
+$conn=mysqli_connect('localhost','root','','Klienci');
+if(!$conn){
+    exit;
+}
+$dodaj=mysqli_query($conn,"INSERT into Klienci Values(12,'Anna','Lisek','Wadowice')");
+if(!$dodaj)
+{
+    mysqli_close($conn);
+    exit;
+}
+$ile=mysqli_affected_rows($conn);
+echo "liczba rekordów dodanych do tabeli to: ".$ile."<br>";
+mysqli_close($conn);
+
+
+<?php
+$conn=mysqli_connect('localhost','root','','Klienci');
+$Imie=$_POST['in'];
+$Nazwisko=$_POST['Na'];
+$Miejscowosc=$_POST['Mi'];
+if(!$conn){
+    exit;
+}
+$zap="INSERT into Klienci (`ID_Klienta`,`Imie`,`Nazwisko`,`Miejscowosc`) Values(NULL,'$Imie','$Nazwisko','$Miejscowosc')";
+$dodaj=mysqli_query($conn,$zap);
+if(!$dodaj)
+{
+    mysqli_close($conn);
+    exit;
+}
+// $ile=mysqli_affected_rows($conn);
+// echo "liczba rekordów dodanych do tabeli to: ".$ile."<br>";
+else{
+    echo "dodano $Nazwisko $Imie z miasta $Miejscowosc";
+}
+mysqli_close($conn);
+?>
+
